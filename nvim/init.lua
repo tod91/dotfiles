1bd1bb0558bddc6674b409b4d08419f777385d36
todor ivanov <todorivanov066@gmail.com>
2025-09-07 18:41:16 +0300

    finished product for now

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,48 +1,40 @@
+require("config.set") -- needs to be before lazy
+require("config.remap")
 require("config.lazy")
-vim.cmd [[colorscheme unokai]]
+
+vim.cmd [[colorscheme desert]]
 
 -- Lua LSP configuration
 vim.lsp.config("lua_ls", {
   -- Command and arguments to start the server.
   cmd = { "lua-language-server" },
-  -- Filetypes to automatically attach to.
   filetypes = { "lua" },
   settings = {
     Lua = {
       diagnostics = {
         disable = { "missing-fields" },
       },
-      globals = { "vim" }, -- Get the language server to recognize the `vim` global
+      globals = { "vim" },
     },
   },
-})  -- <- Closing brace for lua_ls config
+})
 
 -- Go LSP configuration
 vim.lsp.config("gopls", {
   -- Command and arguments to start the server.
   cmd = { "gopls" },
-  -- Filetypes to automatically attach to.
   filetypes = { "go" },
   settings = {
     go = {
       diagnostics = {
         disable = { "missing-fields" },
       },
-      globals = { "vim" }, -- Get the language server to recognize the `vim` global
+      globals = { "vim" }, 
     },
   },
-})  -- <- Closing brace for gopls config
+})
 
 -- Enable language servers
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('gopls')
-
--- General settings for tabs, indentation, and line numbers
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.expandtab = true
-vim.opt.smarttab = true
-vim.opt.softtabstop = 4
-vim.opt.relativenumber = true
-vim.opt.number = true
 
