2655f457523953c701cfc190d8027eaf98cb80f4
todor ivanov <todorivanov066@gmail.com>
2025-09-16 21:36:01 +0300

    [CONFIG] refactored lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,38 +3,3 @@ require("config.remap")
 require("config.lazy")
 
 vim.cmd [[colorscheme desert]]
-
--- Lua LSP configuration
-vim.lsp.config("lua_ls", {
-  -- Command and arguments to start the server.
-  cmd = { "lua-language-server" },
-  filetypes = { "lua" },
-  settings = {
-    Lua = {
-      diagnostics = {
-        disable = { "missing-fields" },
-      },
-      globals = { "vim" },
-    },
-  },
-})
-
--- Go LSP configuration
-vim.lsp.config("gopls", {
-  -- Command and arguments to start the server.
-  cmd = { "gopls" },
-  filetypes = { "go" },
-  settings = {
-    go = {
-      diagnostics = {
-        disable = { "missing-fields" },
-      },
-      globals = { "vim" }, 
-    },
-  },
-})
-
--- Enable language servers
-vim.lsp.enable('lua_ls')
-vim.lsp.enable('gopls')
-
